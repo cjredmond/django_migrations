709281dfbab0f1efe67df6b5284b0aa87acf9bc2
cjredmond <connor.redmond@gmail.com>
2016-10-04 14:36:57 -0400

    final commit

--- django_migrations/models.py
+++ django_migrations/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=20)
+    position = models.CharField(max_length=2)
+    country = models.CharField(max_length=20)
+    rating = models.IntegerField()
+    age = models.IntegerField()
